866d32cc1b5188275913eba24077cc46464bc73d
cjsmithcog <cjsmithcog@gmail.com>
2014-11-03 09:11:04 -0700

    Definition

---
+++ Compiler/Definition_Smith_Werner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace parser
+{
+    public abstract class Definition
+    {
+        public string name;
+    }
+    public abstract class TypeDefinition : Definition
+    {
+    }
+    public class BasicType : TypeDefinition
+    {        
+    }
+    public class Array : TypeDefinition
+    {
+        public TypeDefinition vartype;
+        public int size;
+    }
+    public class VariableDefinition : Definition
+    {
+        public TypeDefinition vartype;
+    }
+    public abstract class SubprogramDefinition : Definition
+    {
+    }
+    public class Procedure : SubprogramDefinition
+    {
+        public System.Collections.Generic.List<VariableDefinition> list;
+    }
+}
